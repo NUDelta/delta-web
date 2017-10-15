e95803c5d037bac4470fe273cd64c135bcda8af1
YK <yk@u.northwestern.edu>
2017-10-15 18:21:52 -0500

    a bunch of changes for delta lab

--- copy.sh
+++ copy.sh
@@ -1,5 +1,8 @@
 cp delta/vision.html core/client/templates/.
-cp delta/router.js core/lib/. 
+cp delta/router.js core/lib/.
 cp delta/layout.html core/client/templates/application/.
 cp delta/paper.html core/client/templates/.
 cp delta/c.js core/collections/.
+cp delta/joinus.html core/client/templates/.
+cp delta/people.js core/server/data/.
+cp delta/dtr.html core/client/.
