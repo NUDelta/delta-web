101a6dcae6acf49afd89566ae664c6fc526e7cf5
Haoqi Zhang <hqz@mit.edu>
2017-10-15 16:39:21 -0500

    added yongsung as admin

--- copy.sh
+++ copy.sh
@@ -2,3 +2,4 @@ cp delta/vision.html core/client/templates/.
 cp delta/router.js core/lib/. 
 cp delta/layout.html core/client/templates/application/.
 cp delta/paper.html core/client/templates/.
+cp delta/c.js core/collections/.
