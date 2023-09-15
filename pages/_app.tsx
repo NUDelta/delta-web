a7d731de4318b09d28242b635cce37ea7363319d
Kapil Garg <kapil1garg@gmail.com>
2023-09-15 15:57:27 -0500

    using next fonts instead of linking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,14 @@ import Head from "next/head";
 import "../styles/globals.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import { Lato } from "next/font/google";
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -13,16 +21,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="google-site-verification"
           content="14ltX0knmJHA_JnO3Z428TX8YioIFSWFy1yABn_Qh10"
         />
-        <link
-          href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic&display=swap"
-          rel="stylesheet"
-          type="text/css"
-        />
-
         <link rel="icon" href="/favicon.ico?v=2" />
       </Head>
 
-      <main className="bg-white pb-8">
+      <main className={`bg-white pb-8 ${lato.className}`}>
         <Component {...pageProps} />
       </main>
     </>
